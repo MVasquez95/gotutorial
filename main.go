55b04343564939aeb6d77d581a66c4d675c9d608
Miguel Arnaldo Vasquez Leon <Miguel.Vasquez@belltech.la>
2023-11-04 08:13:17 -0500

    Condicionales

--- main.go
+++ main.go
@@ -1,13 +1,25 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/gotutorial/variables"
-)
-
 func main() {
-	estado, texto := variables.ConvierteaTexto(1920)
+	//Convertir un entero a texto, se necesita importar package variables
+	/*estado, texto := variables.ConvierteaTexto(1920)
 	fmt.Println(estado)
 	fmt.Println(texto)
+
+	//Condicional
+	if os := runtime.GOOS; os == "Linux." || os == "OS X." {
+		fmt.Println("No estamos usando windows, usamos: ", os)
+	} else {
+		fmt.Println("Estamos usando Windows")
+	}
+
+	//Switch
+	switch os := runtime.GOOS; os {
+	case "linux":
+		fmt.Println("Esto es Linux")
+	case "darwin":
+		fmt.Println("Esto es Darwin")
+	default:
+		fmt.Printf("%s \n", os)
+	}*/
 }
